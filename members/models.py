1d4954de0fa3e6605402267bf6a3fb7497632aba
ilyaskhan15 <ilyaskhanqwer0088@gmail.com>
2024-09-23 23:10:08 +0500

    do some work in templates

--- members/models.py
+++ members/models.py
@@ -4,5 +4,9 @@ class Member(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     phone_number = models.BigIntegerField()
-    join_date = models.DateField()
+    join_date = models.DateField(null=True)
+    
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+    
 # Create your models here.
--- members/views.py
+++ members/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    my_members =  Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': my_members
+    }
+    return HttpResponse(template.render(context, request))
 
 # Create your views here.
