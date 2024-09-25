78f4076c5a658d39d3761ffe2c31488996abf5cb
ilyaskhan15 <ilyaskhanqwer0088@gmail.com>
2024-09-25 11:10:57 +0500

    add home page

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import members, details
+from .views import members, details, main
 
 urlpatterns = [
+    path('', main, name='main'),
     path('members/', members, name='members'),
     path('members/details/<int:id>', details, name='detail' )
 ]
--- members/views.py
+++ members/views.py
@@ -18,4 +18,9 @@ def details(request, id):
     'mymembers': mymembers,
   }
   return HttpResponse(template.render(context, request))
+
+
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
 # Create your views here.
